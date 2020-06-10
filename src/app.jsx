9ba556ca65b616744242b1433d606552e07a4f57
SP-Sumampow <vivian.priska@gmail.com>
2020-06-10 21:53:42 +0200

    add delete movie

--- src/app.jsx
+++ src/app.jsx
@@ -16,6 +16,17 @@ class App extends Component {
     });
   }
 
+  onDeleteClicked(movieId) {
+    let movies = this.state.movies
+    let movieIndex = movies.findIndex((movie) => {
+      return movie.id == movieId
+    })
+    if (movieIndex > -1) {
+      movies.splice(movieIndex, 1);
+    }
+    this.setState({ movies: movies });
+  }
+
   onLikeClicked(movieId) {
     let movies = this.state.movies
     const movie = movies.find((movie) => {
@@ -41,6 +52,7 @@ class App extends Component {
         <MovieComponent
           movie={movie}
           key={movie.id}
+          onDeleteClicked={this.onDeleteClicked.bind(this)}
           onDislikeClicked={this.onDislikeClicked.bind(this)}
           onLikeClicked={this.onLikeClicked.bind(this)}
         />
--- src/movieComponent.jsx
+++ src/movieComponent.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 
-export const MovieComponent = ({ movie, onLikeClicked, onDislikeClicked }) => (
+export const MovieComponent = ({
+  movie,
+  onLikeClicked,
+  onDislikeClicked,
+  onDeleteClicked,
+}) => (
   <div>
     <h1>{movie.title}</h1>
     <span>{movie.category}</span>
     <img src={movie.picture} width="100px" height="auto" />
+    <button
+      type="button"
+      onClick={() => {
+        onDeleteClicked(movie.id);
+      }}
+    >
+      delete
+    </button>
     <button
       type="button"
       onClick={() => {
